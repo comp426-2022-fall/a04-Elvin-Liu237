73a5737603489f0b09b97b84e1984d3ab08204ff
Elvin-Liu237 <elvinliu@live.unc.edu>
2022-11-21 14:07:48 -0500

    t

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ const Dice  = 2;
 const Rolls = 1;
 
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
 'Check endpoint at /app/ that returns 200 OK.'
 app.get('/app/', (req, res) => {
     res.type('html')
-    res.status(200).send('200 OK')
-})
+    res.status(200).send('200 OK');
+});
 
 'Endpoint /app/roll/ that returns JSON for a default roll of two six-sided dice one time.'
 app.get('/app/roll/', (req, res) => {
-    res.send(roll(Sides, Dice, Rolls))
-})
+    res.send(roll(Sides, Dice, Rolls));
+});
 
 'check if JSON or URL encoded'
 
@@ -44,32 +44,32 @@ app.get('/app/roll/', (req, res) => {
         Rolls = parseInt(req.body.rolls)
     }
  
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(roll(Sides, Dice, Rolls))
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send(roll(Sides, Dice, Rolls));
     //res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)))
-})
+});
 
 'Endpoint /app/roll/:sides/ that returns JSON for a default number of rolls'
 app.get('/app/roll/:sides/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), Dice, Rolls))
-})
+    res.send(roll(parseInt(req.params.sides), Dice, Rolls));
+});
 
 'Endpoint /app/roll/:sides/:dice/ that returns JSON for a default number of rolls'
 app.get('/app/roll/:sides/:dice/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), Rolls))
-})
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), Rolls));
+});
 
 'Endpoint /app/roll/:sides/:dice/:rolls/'
 
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
-    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)))
-})
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+});
 
 '404 error not found'
 
 app.get('*', (req, res) => {
-    res.status(404).send("404 NOT FOUND")
-})
+    res.status(404).send("404 NOT FOUND");
+});
 
 app.listen(port);
 
